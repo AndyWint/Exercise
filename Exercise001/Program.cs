a1916542269440f1306c212b263352aa2e4c95ad
[AndyWinter] <avake3d@yandex.ru>
2023-02-13 13:59:14 +0500

    Написал программу поиска большего и меньшего числа

---
+++ Exercise001/Program.cs
@@ -0,0 +1,22 @@
+﻿int numberA = -3;
+int numberB = 8;
+
+Console.Write("Большее число, ");
+    if (numberA > numberB)
+{
+    Console.WriteLine(numberA);
+}
+    else 
+{
+    Console.WriteLine(numberB);
+} 
+
+Console.Write("Меньшее число, ");
+    if (numberA < numberB)
+{
+    Console.WriteLine(numberA);
+}
+    else 
+{
+    Console.WriteLine(numberB);
+}

4ac1a69dba4a3d74505fa5f90bf54bae42a91c0b
[AndyWinter] <avake3d@yandex.ru>
2023-02-16 16:41:53 +0500

    Решение задачи с днями недели

---
+++ Exercise023/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+
+void Wkd(int num)
+{   
+    if (num > 0 && num <= 7)
+    {
+        if (num == 6)
+        {
+            Console.WriteLine("Сегодня выходной!");
+        }
+        else if (num == 7)
+        {
+            Console.WriteLine("Сегодня выходной!");
+        }
+        else
+        {
+            Console.WriteLine("Пора идти на работу T-T");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Это не день недели, введите другое число");
+    }
+
+    
+}
+Console.Write("Введите день недели, ");
+string enter = Console.ReadLine();
+int a = Convert.ToInt32(enter);
+Wkd(a);
+

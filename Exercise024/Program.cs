129fd812243116f5f5509271f46ab7b17cf682c1
[AndyWinter] <avake3d@yandex.ru>
2023-02-23 17:00:00 +0500

    Палиндром, проверка 5 чисел с помощью  метода

--- Exercise024/Program.cs
+++ Exercise024/Program.cs
@@ -1,80 +1,75 @@
 ﻿// Программа проверяет пятизначное число на палиндром
-    
-
-// string[] strs = new string[5];
-// int n = strs.Length;
-// int index = 0;
-// while(index < n)
-// {
-//     Console.Write("Введите число     ", index + 1);
-//     strs[index] = Console.ReadLine();
-//     index++;
-// }
-//
-// Console.WriteLine("Вы ввели следующие числа:");
-
-int [] strs = new int[5];
-strs[0] = 5;
-strs[1] = 4;
-strs[2] = 45;
-strs[3] = 4;
-strs[4] = 5;
-//
-//void PrintArray(int[] col)
-//{
-//    int count= col.Length;
-//    int position = 0;
-//    while(position < count)
-//    {
-//        Console.WriteLine(col[position]);
-//        position++;
-//    }
-//}
-//
-//int Revert(int[] arr)
-//{
-//    int help = 0;
-//    int index = 0;
-//    int n = arr.Length;
-//    while ( index < (n / 2))
-//    {
-//    help = arr[index];
-//    arr[index] = arr [n - 1 - index];
-//    arr[n - 1 - index] = help;
-//    index++;
-//    } 
-//    return arr[index]; 
-//}
-
-//PrintArray(strs);
-//Revert(strs);
-//PrintArray(strs);
 
 
-// if( PrintArray(strs) == Revert(strs))
-// {
-//    Console.WriteLine("Данное число является палиндромом");
-// }
-// else
-// {
-//    Console.WriteLine("Данное число не является палиндромом");
-// }
+
+string[] strs = new string[5];
 int n = strs.Length;
 int index = 0;
-while (index < (n / 2))
+while (index < n)
 {
-    if (strs[4 - index] == strs[0 + index])
+    Console.Write("Введите число ");
+    strs[index] = Console.ReadLine();
+    index++;
+}
+
+Console.Write("Вы ввели следующие числа: ");
+
+
+void PrintArray(string[] col)                  //выводит массив
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
     {
-        index++;
-        
-    }
-    if (strs[4 - index] != strs[0 + index])
-    {
-        Console.WriteLine("Число не является палиндромом");
-        break;
-    }
-    else 
-    {
-        Console.WriteLine("Число является палиндромом");
+        Console.Write(col[position]);
+        Console.Write(" ");
+        position++;
     }
 }
+
+
+string Pal(string[] strs)                      //присваивает значение правда ложь
+{
+    int n = strs.Length - 1;
+    int index = 0;
+    string B = " -1";
+    while (index < n)
+    {
+
+        if (strs[n - index] == strs[0 + index])
+        {
+            index++;
+
+        }
+        if (strs[n - index] != strs[0 + index])
+        {
+            B = "False";
+            break;
+        }
+        else
+        {
+            B = "True";
+        }
+    }
+
+
+    return B;
+}
+
+
+void Trfs(string B)                        //выводит надпись 
+{
+    string A = B;
+    if (A == "True")
+    {
+        Console.WriteLine(" Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine(" Число не является палиндромом");
+    }
+}
+
+
+PrintArray(strs);
+Trfs(Pal(strs));

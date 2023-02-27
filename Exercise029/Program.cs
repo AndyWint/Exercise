d0fd167ba0eef38921d2e71393c807257116ad9f
[AndyWinter] <avake3d@yandex.ru>
2023-02-27 19:41:12 +0500

    Массив из n элементов с выводом

---
+++ Exercise029/Program.cs
@@ -0,0 +1,33 @@
+﻿//  Напишите метод, который задаёт массив из N элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void Mass1(double[] strs)       //задает массив
+{
+    int n = strs.Length;
+    int index = 0;
+    while (index < n)
+    {
+        Console.Write("Введите число ");
+        strs[index] = double.Parse(Console.ReadLine());
+        index++;
+    }
+}
+
+void PrintArray(double[] col)                  //выводит массив
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        Console.Write(" ");
+        position++;
+    }
+}
+
+//double A = Console.ReadLine("Введите число");
+double[] arr = new double[5];
+Mass1(arr);
+PrintArray(arr);
+

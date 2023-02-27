edf6c3fd504ed3ca30b07f3999e64ae90526eecf
[AndyWinter] <avake3d@yandex.ru>
2023-02-27 19:23:22 +0500

    Сумма цифр в числе

---
+++ Exercise028/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Number(string mes)
+{
+    Console.WriteLine(mes);
+    int A = int.Parse(Console.ReadLine());
+    return A;
+}
+
+int Sum(int number)
+{
+    int res = 0;
+    while (number > 0)
+    {
+        res = res + number % 10;
+        number = number / 10;
+    }
+    return res;
+
+}
+void Print(int A)
+{
+    Console.WriteLine(A);
+}
+
+int A = Number("Введите число");
+Print(Sum(A));
